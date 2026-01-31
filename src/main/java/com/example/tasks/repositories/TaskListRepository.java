5299f2f180963477410fa146142224137013fc84
MohitSinha11 <sinhamohit1122@gmail.com>
2026-01-31 13:07:45 +0530

    Created Repository , Service and Controller (With 1 GetMapping)

---
+++ src/main/java/com/example/tasks/repositories/TaskListRepository.java
@@ -0,0 +1,14 @@
+package com.example.tasks.repositories;
+
+import com.example.tasks.domain.entities.TaskList;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+
+@Repository
+public interface TaskListRepository extends JpaRepository<TaskList, UUID> {
+    //Jpa is accepting The entity and its Primary Key
+}
---
+++ src/main/java/com/example/tasks/repositories/TaskRepository.java
@@ -0,0 +1,16 @@
+package com.example.tasks.repositories;
+
+import com.example.tasks.domain.entities.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, UUID> {
+    //Custom Queries
+    List<Task> findByTaskListId(UUID taskListId);
+    Optional<Task> findByTaskListIdAndId(UUID taskListId , UUID Id);
+}
