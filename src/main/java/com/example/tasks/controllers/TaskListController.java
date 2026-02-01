9d28d7f3ddb6fa6e648211c54483f97fbfaa0804
MohitSinha11 <sinhamohit1122@gmail.com>
2026-02-01 23:16:55 +0530

    Added Update Task list and delete task list end points

--- src/main/java/com/example/tasks/controllers/TaskListController.java
+++ src/main/java/com/example/tasks/controllers/TaskListController.java
@@ -44,4 +44,23 @@ public class TaskListController {
         );
         return taskListMapper.toDto(createdTaskList);
     }
+
+    @PutMapping(path = "/{task_list_id}")
+    public TaskListDto updateTaskList(
+            @PathVariable("task_list_id")UUID taskListId,
+            @RequestBody TaskListDto taskListDto
+    ){
+        TaskList updatedTaskList = taskListService.updateTaskList(
+                taskListId,
+                taskListMapper.fromDto(taskListDto)
+        );
+        return taskListMapper.toDto(updatedTaskList);
+    }
+
+    @DeleteMapping(path = "/{task_list_id}")
+    public void deleteTaskList(
+            @PathVariable("task_list_id")UUID taskListId
+    ){
+        taskListService.deleteTaskList(taskListId);
+    }
 }
--- src/main/java/com/example/tasks/services/impl/TaskListServiceImpl.java
+++ src/main/java/com/example/tasks/services/impl/TaskListServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -52,5 +53,34 @@ public class TaskListServiceImpl implements TaskListService {
     @Override
     public Optional<TaskList> getTaskList(UUID id) {
         return taskListRepository.findById(id);
+    }
+
+    @Override
+    public TaskList updateTaskList(UUID taskListId, TaskList taskList) {
+        if(taskList.getId() == null)
+            throw new IllegalArgumentException(
+                    "Task list must have an ID"
+            );
+
+        if(!Objects.equals(taskList.getId(),taskListId))
+            throw new IllegalArgumentException(
+                    "Attempting to change task list id , This is not permitted!"
+            );
+
+        TaskList existingTaskList = taskListRepository.findById(taskListId).orElseThrow(
+                ()->new IllegalArgumentException(
+                        "Task List not found!"
+                )
+        );
+
+        existingTaskList.setTitle(taskList.getTitle());
+        existingTaskList.setDescription(taskList.getDescription());
+        existingTaskList.setUpdated(LocalDateTime.now());
+        return taskListRepository.save(existingTaskList);
+    }
+
+    @Override
+    public void deleteTaskList(UUID taskListId) {
+        taskListRepository.deleteById(taskListId);
     }
 }
