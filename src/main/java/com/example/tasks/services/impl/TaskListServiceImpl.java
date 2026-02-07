f7077c2bc8dbdf6790ef715cba59c6b66222e207
MohitSinha11 <sinhamohit1122@gmail.com>
2026-02-07 12:44:26 +0530

    added @Transactional annotation because the update service call the repository multiple time find and save call which can lead to some data issues thats why to use the rollback property we used it.

--- src/main/java/com/example/tasks/services/impl/TaskListServiceImpl.java
+++ src/main/java/com/example/tasks/services/impl/TaskListServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.tasks.services.impl;
 import com.example.tasks.domain.entities.TaskList;
 import com.example.tasks.repositories.TaskListRepository;
 import com.example.tasks.services.TaskListService;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -55,6 +56,7 @@ public class TaskListServiceImpl implements TaskListService {
         return taskListRepository.findById(id);
     }
 
+    @Transactional //making multiple calls find and save
     @Override
     public TaskList updateTaskList(UUID taskListId, TaskList taskList) {
         if(taskList.getId() == null)
