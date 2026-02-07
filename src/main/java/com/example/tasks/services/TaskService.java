47529e59a765edb9d4007227819b6bd3c339c69b
MohitSinha11 <sinhamohit1122@gmail.com>
2026-02-07 10:53:15 +0530

    Added new method for Creating new task

---
+++ src/main/java/com/example/tasks/services/TaskService.java
@@ -0,0 +1,12 @@
+package com.example.tasks.services;
+
+import com.example.tasks.domain.entities.Task;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface TaskService {
+    List<Task> listTasks(UUID taskListId);
+    Task createTask(UUID taskListId , Task task);
+
+}
---
+++ src/main/java/com/example/tasks/services/impl/TaskServiceImpl.java
@@ -0,0 +1,69 @@
+package com.example.tasks.services.impl;
+
+import com.example.tasks.domain.entities.Task;
+import com.example.tasks.domain.entities.TaskList;
+import com.example.tasks.domain.entities.TaskPriority;
+import com.example.tasks.domain.entities.TaskStatus;
+import com.example.tasks.repositories.TaskListRepository;
+import com.example.tasks.repositories.TaskRepository;
+import com.example.tasks.services.TaskService;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class TaskServiceImpl implements TaskService {
+
+    private final TaskRepository taskRepository;
+    private final TaskListRepository taskListRepository;
+
+    public TaskServiceImpl(TaskRepository taskRepository, TaskListRepository taskListRepository) {
+        this.taskRepository = taskRepository;
+        this.taskListRepository = taskListRepository;
+    }
+
+    @Override
+    public List<Task> listTasks(UUID taskListId) {
+       return taskRepository.findByTaskListId(taskListId);
+    }
+
+    @Override
+    public Task createTask(UUID taskListId, Task task) {
+        if(task.getId() != null)
+            throw new IllegalArgumentException(
+                    "Task already has an ID!"
+            );
+
+        if(task.getTitle() == null || task.getTitle().isBlank())
+            throw new IllegalArgumentException(
+                    "Task must have a Title!"
+            );
+
+        TaskPriority taskPriority = Optional.ofNullable(task.getPriority())
+                .orElse(TaskPriority.MEDIUM);
+
+        TaskStatus taskStatus = TaskStatus.OPEN;
+
+        TaskList taskList = taskListRepository.findById(taskListId)
+                .orElseThrow(() -> new IllegalArgumentException(
+                        "Invalid Task list ID provided!"
+                ));
+
+        Task taskToSave = new Task(
+                null,
+                task.getTitle(),
+                task.getDescription(),
+                task.getDueDate(),
+                taskStatus,
+                taskPriority,
+                taskList,
+                LocalDateTime.now(),
+                LocalDateTime.now()
+        );
+
+        return taskRepository.save(taskToSave);
+    }
+}
